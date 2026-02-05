7f952d150377c7a300c03171c342308a0763fa34
edugrowth5-rgb <edugrowth5@gmail.com>
2026-02-05 19:20:38 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,36 +1,16 @@
-const cacheName = 'sagar-v11';
-const assets = [
-  './',
-  './index.html',
-  './manifest.json'
-];
+const cacheName = 'sagar-v12';
+const assets = ['./', './index.html', './manifest.json'];
 
-// Naya version install karne ke liye
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(cacheName).then(cache => {
-      console.log('Sagar Clock: Caching New Version');
-      return cache.addAll(assets);
-    })
-  );
+  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
 });
 
-// Purana cache delete karne ke liye
 self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(keys
-        .filter(key => key !== cacheName)
-        .map(key => caches.delete(key))
-      );
-    })
-  );
+  e.waitUntil(caches.keys().then(keys => {
+    return Promise.all(keys.filter(k => k !== cacheName).map(k => caches.delete(k)));
+  }));
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(res => {
-      return res || fetch(e.request);
-    })
-  );
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
